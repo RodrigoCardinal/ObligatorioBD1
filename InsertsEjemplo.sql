31a751d75b523a0b263e1dd8a4c68143d69fd2f3
monicadeus <deusmonica06@gmail.com>
2025-11-12 21:30:02 -0300

    app

--- InsertsEjemplo.sql
+++ InsertsEjemplo.sql
@@ -1,7 +1,6 @@
 USE ObligatorioBD1;
--- UPDATE login SET contraseña = MD5(contraseña);
--- Tabla: login
-INSERT INTO login (correo, contraseña, es_administrador) VALUES
+
+INSERT INTO ObligatorioBD1.login (correo, contraseña, es_administrador) VALUES
 ('administrador@ucu.edu.uy', 'admin2025',True),
 ('usuario@ucu.edu.uy', 'user2025',False),
 ('juan.perez@ucu.edu.uy', 'Jperez#2025!',False),
@@ -11,20 +10,20 @@ INSERT INTO login (correo, contraseña, es_administrador) VALUES
 ('sofia.fernandez@ucu.edu.uy', 'SofiFz_2025!',False),
 ('martin.lopez@ucu.edu.uy', 'Mlopez#77*',False);
 -- Tabla: facultad
-INSERT INTO facultad (id_facultad, nombre) VALUES
+INSERT INTO ObligatorioBD1.facultad (id_facultad, nombre) VALUES
 (1, 'Facultad de Ingeniería y Tecnologías'),
 (2, 'Facultad de Ciencias Empresariales'),
 (3, 'Facultad de Psicología'),
 (4, 'Facultad de Derecho');
 
 -- Tabla: edificio
-INSERT INTO edificio (nombre_edificio, direccion, departamento) VALUES
+INSERT INTO ObligatorioBD1.edificio (nombre_edificio, direccion, departamento) VALUES
 ('Edificio Central', 'Av. 8 de Octubre 2738', 'Montevideo'),
 ('Edificio de Ingeniería', 'Bvar. Artigas 1514', 'Montevideo'),
 ('Campus Punta del Este', 'Av. Roosevelt 1234', 'Maldonado');
 
 -- Tabla: programa_académico
-INSERT INTO programa_academico (nombre_programa, id_facultad, tipo) VALUES
+INSERT INTO ObligatorioBD1.programa_academico (nombre_programa, id_facultad, tipo) VALUES
 ('Ingeniería Informática', 1, 'grado'),
 ('Administración de Empresas', 2, 'grado'),
 ('Psicología Clínica', 3, 'grado'),
@@ -33,7 +32,7 @@ INSERT INTO programa_academico (nombre_programa, id_facultad, tipo) VALUES
 ('Psicología Organizacional', 3, 'posgrado');
 
 -- Tabla: participante
-INSERT INTO participante(ci, nombre, apellido, email) VALUES
+INSERT INTO ObligatorioBD1.participante(ci, nombre, apellido, email) VALUES
 (49382716, 'Juan', 'Pérez', 'juan.perez@ucu.edu.uy'),
 (51293487, 'María', 'Gómez', 'maria.gomez@ucu.edu.uy'),
 (47851239, 'Ana', 'Rodríguez', 'ana.rodriguez@ucu.edu.uy'),
@@ -42,7 +41,7 @@ INSERT INTO participante(ci, nombre, apellido, email) VALUES
 (52349872, 'Martín', 'López', 'martin.lopez@ucu.edu.uy');
 
 -- Tabla: participante_programa_académico
-INSERT INTO participante_programa_academico (id_alumno_programa, ci_participante, nombre_programa, rol) VALUES
+INSERT INTO ObligatorioBD1.participante_programa_academico (id_alumno_programa, ci_participante, nombre_programa, rol) VALUES
 (1, 49382716, 'Ingeniería Informática', 'alumno'),
 (2, 51293487, 'Administración de Empresas', 'alumno'),
 (3, 47851239, 'Psicología Clínica', 'alumno'),
@@ -51,7 +50,7 @@ INSERT INTO participante_programa_academico (id_alumno_programa, ci_participante
 (6, 52349872, 'Psicología Organizacional', 'docente');
 
 -- Tabla: sala
-INSERT INTO sala (nombre_sala, edificio, capacidad, tipo_sala) VALUES
+INSERT INTO ObligatorioBD1.sala (nombre_sala, edificio, capacidad, tipo_sala) VALUES
 ('Lab A', 'Edificio de Ingeniería', 30, 'libre'),
 ('Sala 101', 'Edificio Central', 50, 'libre'),
 ('Aula Magna', 'Edificio Central', 100, 'libre'),
@@ -59,7 +58,7 @@ INSERT INTO sala (nombre_sala, edificio, capacidad, tipo_sala) VALUES
 ('Sala Docente 2', 'Edificio de Ingeniería', 10, 'docente');
 
 -- Tabla: turno
-INSERT INTO turno (hora_inicio, hora_fin) VALUES
+INSERT INTO ObligatorioBD1.turno (hora_inicio, hora_fin) VALUES
 ('08:00', '09:00'),
 ('09:00', '10:00'),
 ('10:00', '11:00'),
@@ -76,7 +75,7 @@ INSERT INTO turno (hora_inicio, hora_fin) VALUES
 ('22:00', '23:00');
 
 -- Tabla: reserva
-INSERT INTO reserva (id_reserva, nombre_sala, edificio, fecha, id_turno, estado) VALUES
+INSERT INTO ObligatorioBD1.reserva (id_reserva, nombre_sala, edificio, fecha, id_turno, estado) VALUES
 (1, 'Lab A', 'Edificio de Ingeniería', '2025-09-01', 1, 'activa'),
 (2, 'Sala 101', 'Edificio Central', '2025-09-01', 2, 'cancelada'),
 (3, 'Aula Magna', 'Edificio Central', '2025-09-02', 3, 'finalizada'),
@@ -86,7 +85,7 @@ INSERT INTO reserva (id_reserva, nombre_sala, edificio, fecha, id_turno, estado)
 (7, 'Sala 101', 'Edificio Central', '2025-09-05', 4, 'activa');
 
 -- Tabla: reserva_participante
-INSERT INTO reserva_participante (ci_participante, id_reserva, fecha_solicitud_reserva, asistencia) VALUES
+INSERT INTO ObligatorioBD1.reserva_participante (ci_participante, id_reserva, fecha_solicitud_reserva, asistencia) VALUES
 (49382716, 1, '2025-08-28', true),
 (51293487, 2, '2025-08-28', false),
 (47851239, 3, '2025-08-29', true),
@@ -97,14 +96,14 @@ INSERT INTO reserva_participante (ci_participante, id_reserva, fecha_solicitud_r
 (51293487, 7, '2025-09-02', true);
 
 -- Tabla: sancion_participante
-INSERT INTO sancion_participante (ci_participante, fecha_inicio, fecha_fin) VALUES
+INSERT INTO ObligatorioBD1.sancion_participante (ci_participante, fecha_inicio, fecha_fin) VALUES
 (51293487, '2025-09-05', '2025-11-05'),
 (49382716, '2025-09-10', '2025-11-10'),
 (52349872, '2025-09-08', '2025-11-08');
 
 
 -- Tabla: invitados
-INSERT INTO invitados(ci_invitado,responsable_ci,nombre_invitado,apellido_invitado,email,id_reserva, id_turno) VALUES
+INSERT INTO ObligatorioBD1.invitados(ci_invitado,responsable_ci,nombre_invitado,apellido_invitado,email,id_reserva, id_turno) VALUES
 (43459814,50128394,'Julian','Perez','julian.perez@gmail.com',1,1),
 (56784229,51293487,'Ana','Gonzalez','ana.gonzalez@gmail.com',7,4),
 (93245881,48927153,'Juliana','Mendez','juliana.mendez@gmail.com',4,1);
--- app.py
+++ app.py
@@ -823,3 +823,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
